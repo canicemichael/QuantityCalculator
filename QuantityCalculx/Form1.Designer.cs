cdbd8062f78d0caa019fd36600f2c486a3537e93
canice michael <50200115+canicemichael@users.noreply.github.com>
2021-06-20 12:06:38 +0100

    Folder containing quantity calculator app

---
+++ QuantityCalculx/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿
+namespace QuantityCalculx
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.EffCsttb = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Incometb = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Input EffectiveCost";
+            // 
+            // EffCsttb
+            // 
+            this.EffCsttb.Location = new System.Drawing.Point(145, 41);
+            this.EffCsttb.Name = "EffCsttb";
+            this.EffCsttb.Size = new System.Drawing.Size(120, 23);
+            this.EffCsttb.TabIndex = 1;
+            this.EffCsttb.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.EffCsttb_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "EffectiveCost List";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(145, 88);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 94);
+            this.listBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(305, 41);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(305, 88);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Remove";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(35, 215);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Input Income";
+            // 
+            // Incometb
+            // 
+            this.Incometb.Location = new System.Drawing.Point(145, 212);
+            this.Incometb.Name = "Incometb";
+            this.Incometb.Size = new System.Drawing.Size(120, 23);
+            this.Incometb.TabIndex = 7;
+            this.Incometb.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Incometb_KeyPress);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(305, 211);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Calculate";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(35, 263);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 15);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Xqty(1-n) = ";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(305, 263);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "ClearAll";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.HorizontalScrollbar = true;
+            this.listBox2.ItemHeight = 15;
+            this.listBox2.Location = new System.Drawing.Point(112, 263);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(176, 109);
+            this.listBox2.TabIndex = 12;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(305, 130);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(108, 23);
+            this.button5.TabIndex = 13;
+            this.button5.Text = "Remove All Costs";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(507, 424);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.Incometb);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.EffCsttb);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox EffCsttb;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox Incometb;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ QuantityCalculx/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+
+namespace QuantityCalculx
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //if (this.EffCsttb.Text != "")
+            
+            if(int.TryParse(this.EffCsttb.Text, out int space))
+            {
+                listBox1.Items.Add(this.EffCsttb.Text);
+                this.EffCsttb.Focus();
+                this.EffCsttb.Clear();
+            }            
+        }
+
+        
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.RemoveAt(listBox1.Items.Count - 1);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            /*
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();
+            foreach(object item in listBox1.Items)
+            {
+                sb.Append((double)Convert.ToInt32(Incometb.Text) / (listBox1.Items.Count * Convert.ToInt32(item)));
+                sb.Append("\n");
+                sb.Append("\n");
+            }
+            sb.ToString();
+            int x = int.Parse(sb.ToString());
+            listBox2.Items.Add(x);
+            */
+            //double sum = 0;
+            listBox2.Items.Clear();
+
+            List<double> sum = new List<double>();
+
+            if(int.TryParse(Incometb.Text, out int cloud))
+            {
+                for (int i = 0; i < listBox1.Items.Count; i++)
+                {
+                    sum.Add(Convert.ToDouble(Incometb.Text) / (listBox1.Items.Count * Convert.ToDouble(listBox1.Items[i])));
+                }
+            }
+            
+            for (int j = 0; j < listBox1.Items.Count; j++)
+            {
+                listBox2.Items.Add("Xqty"+(j+1) +" = "+ sum[j]);
+            }
+            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            EffCsttb.Text = "";
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            Incometb.Text = "";
+        }
+
+        private void EffCsttb_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == '\r')
+            {
+                button1_Click(this, new EventArgs());
+                e.Handled = true;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+
+        private void Incometb_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == '\r')
+            {
+                button3_Click(this, new EventArgs());
+                e.Handled = true;
+            }
+        }
+    }
+}
